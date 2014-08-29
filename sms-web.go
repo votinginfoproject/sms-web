b2387fbd635f03b5fbefb1bd667435c3a389f30f
Joshua Antonishen <joshua@antonishen.com>
2014-08-29 15:50:20 -0500

    set gomaxprocs

--- sms-web.go
+++ sms-web.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"runtime"
+	"strconv"
 
 	"github.com/votinginfoproject/sms-web/env"
 	"github.com/votinginfoproject/sms-web/queue"
@@ -11,6 +14,12 @@ import (
 
 func main() {
 	env.Load()
+
+	procs, err := strconv.Atoi(os.Getenv("PROCS"))
+	if err != nil {
+		log.Fatal("[ERROR] you must specify procs in the .env file")
+	}
+	runtime.GOMAXPROCS(procs)
 
 	q := queue.New()
 	log.Panic(http.ListenAndServe(":8080", routes.New(q)))
