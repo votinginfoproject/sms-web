e6eb4bed47d94d9fb40605c7ad43895d45eff691
Joshua Antonishen <joshua@antonishen.com>
2014-08-29 16:25:54 -0500

    log msg formatting

--- routes/routes.go
+++ routes/routes.go
@@ -18,7 +18,7 @@ type Server struct {
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	log.Print(fmt.Sprintf("[INFO] [REQUEST] : Method: %s - Path: %s - Host: %s, FormData: %s", r.Method, r.URL.RequestURI(), r.Host, r.Form))
+	log.Print(fmt.Sprintf("[INFO] [REQUEST] Method: %s - Path: %s - Host: %s, FormData: %s", r.Method, r.URL.RequestURI(), r.Host, r.Form))
 
 	s.handler.ServeHTTP(w, r)
 }
